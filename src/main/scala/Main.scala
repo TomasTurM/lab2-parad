596818004754749fe8af0769d5e7577fbaa1fa0b
TomasTurM <tomy.monier@gmail.com>
2026-05-10 23:52:29 -0300

    min fixes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,10 +27,6 @@ object Main {
     // ------------------------------------------------------------------
     // Paso 3: Detectar entidades y mostrar resultados por post
     // ------------------------------------------------------------------
-    // TODO (Ejercicios 3, 4 y 6):
-    //   Para cada post:
-    //     1. Detectar entidades
-    //     2. Formatear y mostrar el resultado
     allPosts.foreach{ case (_, titles) => 
       titles.foreach{ title =>
         val entities = Analyzer.detectEntities(title, dictionary)
@@ -43,16 +39,11 @@ object Main {
     // ------------------------------------------------------------------
     // Paso 4: Estadísticas globales
     // ------------------------------------------------------------------
-    // TODO (Ejercicios 5 y 6):
-    //   1. Recolectar TODAS las entidades detectadas en todos los posts
-    //   2. Contar por tipo
-    //   3. Mostrar el resumen
     val entities = allPosts.flatMap{ case (_, titles) => 
       titles.flatMap{ title =>
         Analyzer.detectEntities(title, dictionary)
       }
     }
-    // }.distinct
 
     val counts = Analyzer.countByType(entities)
     val stats = Formatters.formatEntityStats(counts)
